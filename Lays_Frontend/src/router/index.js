a768cfaff053980638a24e3b5132fcf4bb80a8ed
HyphaSlayer <tiebo.croons@gmail.com>
2025-12-01 12:10:23 +0100

    Add configurator views and chips bag model
    - Introduced new GLB model for chips bag in the configurator.
    - Updated router to include paths for Configurator and ConfiguratorVue views.
    - Added package-lock.json to track dependencies.

--- Lays_Frontend/src/router/index.js
+++ Lays_Frontend/src/router/index.js
@@ -3,6 +3,8 @@ import BagDetail from '../views/BagDetail.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import CreateBag from '../views/CreateBag.vue';
+import Configurator from '../views/Configurator.vue';
+import ConfiguratorVue from '../views/ConfiguratorVue.vue';
 
 export default [
   { path: '/', redirect: '/bags' },
@@ -11,4 +13,6 @@ export default [
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/create', component: CreateBag }
+  ,{ path: '/configurator', component: Configurator }
+  ,{ path: '/configurator-vue', component: ConfiguratorVue }
 ];
