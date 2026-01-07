798bf205e2f3409c3f6472ccd4fd9412c8f7e348
HyphaSlayer <tiebo.croons@gmail.com>
2026-01-07 01:11:11 +0100

    fix: update API_BASE to use the correct production URL

--- Lays_Frontend/src/api.js
+++ Lays_Frontend/src/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || (import.meta.env.DEV ? 'http://localhost:4000/api/v1' : '/api/v1');
+const API_BASE =
+  import.meta.env.VITE_API_BASE ||
+  (import.meta.env.DEV
+    ? 'http://localhost:4000/api/v1'
+    : 'https://lays-api-f5e978f9aa73.herokuapp.com/api/v1');
 
 const api = axios.create({
   baseURL: API_BASE,
